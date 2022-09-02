81c0b202da4ce8d0f480a71f509c82d40628a0c2
Mohammod Ali <mohammodali651@gmail.com>
2022-09-03 01:44:42 +0600

    show the news categories

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
-console.log('this is working')
+const loadNewsCategory = async() => {
+    const url = `https://openapi.programming-hero.com/api/news/categories`
+    
+    const res = await fetch(url);
+    const data = await res.json();
+    displayNewsCategory(data.data.news_category)
+}
+
+const displayNewsCategory =  news_categories =>{
+    const newsContainer = document.getElementById('news-category');
+    news_categories.forEach(news_category => {
+        const newsDiv = document.createElement('div')
+        newsDiv.classList.add('p-1')
+        newsDiv.innerHTML= `
+            <p>${news_category.category_name}</p>
+        `;
+        newsContainer.appendChild(newsDiv)
+    })
+}
+
+loadNewsCategory()
