525981c65d0b79e79d428063d97cee19bc241fab
Mohammod Ali <mohammodali651@gmail.com>
2022-09-03 04:46:45 +0600

    card added

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ const loadNewsCategory = async() => {
     displayNewsCategory(data.data.news_category)
 }
 
-const displayNewsCategory =  news_categories =>{
+const displayNewsCategory =  news_categories => {
 
     const newsContainer = document.getElementById('news-category');
 
@@ -22,13 +22,59 @@ const displayNewsCategory =  news_categories =>{
 }
 
 const loadNewsApi = async(category_id)=>{
-    console.log(category_id)
+    // console.log(category_id)
     const url = `https://openapi.programming-hero.com/api/news/category/${category_id}`
     const res = await fetch(url);
     const data = await res.json();
-    console.log(data.data)
+    displayNewsCard(data.data)
 }
 
+const displayNewsCard = news_cards => {
+    console.log(news_cards)
+    const cardContainer = document.getElementById('card-container')
+
+    news_cards.forEach(news_card =>{
+        const cardDiv = document.createElement('div')
+        cardDiv.classList.add('col')
+        cardDiv.innerHTML = `
+        <div class="card">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+        </div>
+        `
+        cardContainer.appendChild(cardDiv)
+    })
+
+}
+
+// const displayNews = newsCards => {
+
+//     const newsSection = document.getElementById('news-section')
+    
+//     newsCards.forEach(newsCard => {
+//         console.log(newsCard)
+//     const cardDiv = document.createElement('div')
+//     cardDiv.classList.add('row g-0')
+
+//     cardDiv.innerHTML = `
+//     <div class="col-md-4">
+//         <img src="..." class="img-fluid rounded-start" alt="...">
+//     </div>
+//     <div class="col-md-8">
+//         <div class="card-body">
+//             <h5 class="card-title">Card title</h5>
+//             <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+//             <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+//         </div>
+//     </div>
+//     `;
+//     })
+    
+//     newsSection.appendChild(cardDiv)
+// }
 
 
 loadNewsCategory()
